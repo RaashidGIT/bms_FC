0cebe579d213e0aa5e906a70c796987699114c42
RaashidGIT <muhdraashid@gmail.com>
2024-02-26 16:26:07 +0530

    Added Mapbox to the app in the 'map' drawer.

--- lib/Default_page/my_location_map.dart
+++ lib/Default_page/my_location_map.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
 
-class MyLocationMap extends StatefulWidget {
-  const MyLocationMap({super.key});
+class MyLocationMap extends StatelessWidget {
+  const MyLocationMap({Key? key}) : super(key: key);
 
-  @override
-  State<MyLocationMap> createState() => _MyLocationMapState();
-}
-
-class _MyLocationMapState extends State<MyLocationMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My location'),
+        title: const Text('My Location'), // Set your desired title here
       ),
-      body: const Center(
-        child: Text('Your Location'),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: latLng.LatLng(51.505, -0.09), // Center coordinates (latitude, longitude)
+          initialZoom: 13.0, // Initial zoom level
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://api.mapbox.com/styles/v1/raashidmap/clt2s6qi000ld01qz7remhmr1/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmFhc2hpZG1hcCIsImEiOiJjbHQycm16d2wxanlvMmlsb25kNml0M213In0.3sL2VMG5KDjM7wfgcpuVnQ",
+            additionalOptions: {
+              'accessToken': 'pk.eyJ1IjoicmFhc2hpZG1hcCIsImEiOiJjbHQycm16d2wxanlvMmlsb25kNml0M213In0.3sL2VMG5KDjM7wfgcpuVnQ',
+              'id': 'mapbox.mapbox-streets-v8',
+            },
+          ),
+        ],
       ),
     );
   }
